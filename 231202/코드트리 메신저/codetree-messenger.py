5a07c42f107a0aa09db0afc4a5466b5e0c357e90
한현민 <33598545+Hyunmin-H@users.noreply.github.com>
2023-12-02 20:15:59 +0900

    Title: 코드트리 메신저, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231202/코드트리 메신저/codetree-messenger.py
@@ -0,0 +1,80 @@
+# import sys
+
+def make_tree_and_authorities(node_info):
+    tree = {}
+    tree[0] = [None]
+    for n in range(1, N+1):
+        tree[n] = [node_info[n]]
+        tree[node_info[n]].append(n)
+
+    authorities=[None]
+    for n in range(N+1, 2*N+1):
+        authorities.append(node_info[n])
+    return tree, authorities
+
+def change_alarm(n):
+
+    alarms[n] = not alarms[n]
+
+    return alarms
+
+def change_authority(n, power):
+    authorities[n] = power
+
+def change_nodes(a, b):
+
+    tree[tree[a][0]].remove(a)
+    tree[tree[a][0]].append(b)
+
+    tree[tree[b][0]].remove(b)
+    tree[tree[b][0]].append(a)
+    #
+    temp = tree[a][0]
+    tree[a][0] = tree[b][0]
+    tree[b][0] = temp
+
+
+def calc_numChats(n, depth):
+    global answer
+    if not alarms[n] :
+        return
+    if authorities[n] >= depth :
+        answer += 1
+
+    for nn in tree[n][1:]:
+        calc_numChats(nn, depth+1)
+def print_numChats(n, depth):
+
+    global answer
+    for nn in tree[n][1:]:
+        calc_numChats(nn, depth+1)
+    print(answer)
+
+
+
+
+# sys.stdin = open("/Users/hanhyunmin/PycharmProjects/pythonProject/코드트리메신저/input.txt", "r")
+# T = int(input())
+
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+# for test_case in range(1, T + 1):
+
+N, Q = map(int, input().split())
+node_info = list(map(int, input().split()))
+tree, authorities = make_tree_and_authorities(node_info)
+alarms = [True for _ in range(N+1)]
+answer = 0
+for qs in range(Q-1) :
+    q = list(map(int, input().split()))
+
+    if q[0] == 200 :
+        change_alarm(q[1])
+    elif q[0] == 300 :
+        change_authority(q[1], q[2])
+    elif q[0] == 400 :
+        change_nodes(q[1], q[2])
+    elif q[0] == 500 :
+        print_numChats(q[1], 0)
+        answer = 0
+    # if qs == 4 :
+    #     break
